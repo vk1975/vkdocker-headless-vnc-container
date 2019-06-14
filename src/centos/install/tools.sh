0dcc685a84ce539cafbc82a862f25d4db236e412
V. Kumar <vkumar@calysta.com>
2019-06-14 16:00:39 -0700

    added

--- src/centos/install/tools.sh
+++ src/centos/install/tools.sh
@@ -7,4 +7,5 @@ yum -y install epel-release
 yum -y update
 yum -y install vim sudo wget which net-tools bzip2 \
     numpy #used for websockify/novnc
+yum install -y openssh-server openssh-clients
 yum clean all
--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -6,6 +6,7 @@ echo "Install some common tools for further installation"
 apt-get update 
 apt-get install -y vim wget net-tools locales bzip2 \
     python-numpy #used for websockify/novnc
+apt-get install -y openssh-server 
 apt-get clean -y
 
 echo "generate locales für en_US.UTF-8"
